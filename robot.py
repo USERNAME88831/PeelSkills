e5bef9d3a28c47a31dcdf6c30b32588c7bb47bcd
Owner <786990@pdsb.net>
2023-02-25 19:12:28 -0500

    modified:   robot.py

--- robot.py
+++ robot.py
@@ -15,17 +15,26 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
 class ROBOT(): #TODO: finish making basic functions, and add a line tracking system here.
-    def __init__(self, M1Port, M2Port, M3Port, ColorSensorPort, FrontSensorPort, LeftSensorPort, RightSensorPort, debugMode = False):
+    def __init__(self, M1Port, M2Port, M3Port, ColorSensorPort, FrontSensorPort, LeftSensorPort, RightSensorPort, debugMode = False, overrideSafetyFeatures = False):
         """
         """
         self.wheelDiameter = 1 # the diameter of the wheels
         self.axleTrack = 1 # the horizontal distance between the two wheels, practically the width of the robot  
         # TODO: get diameter of the two wheels, and horizontal distance between the two wheels
 
-        
+        self.safeMode = not overrideSafetyFeatures # safety features should only be turned off while testing and debugging
+
+        self.obstacleDetected = False # when the robot comes to a complete stop because of saftey features, this boolean is set to true
+        # NOTE: when the obstacle isn't a problem anymore, the program has to manually set it to false
+
         self.debugMode = debugMode # enables certain features to test the robot.
-        self.safeDistance = 300 # avoid the obstacle when it reaches this distance(uses mm)
+
+
+
         self.brick = EV3Brick()
+        self.slowDownDistance = 500 # slows down when it is near this distance
+        self.stopDistance = 200 # comes to a complete stop when it reaches this distance
+
         self.LeftWheel = Motor(M1Port)
         self.RightWheel = Motor(M2Port)
         self.motor = DriveBase(self.LeftWheel, self.RightWheel, self.wheelDiameter, self.axleTrack) # The class used to drive robots
@@ -47,7 +56,9 @@ class ROBOT(): #TODO: finish making basic functions, and add a line tracking sys
     def backward(self, distance):
         self.motor.straight(-distance)
 
-    def followLine(self, speed, threshold):
+    
+
+    def followLine(self, speed, threshold) -> int:
         
         """ 
         This function should be put in a loop in order to fully work, and when finished make sure to end with self.motor.stop().\n
@@ -59,12 +70,23 @@ class ROBOT(): #TODO: finish making basic functions, and add a line tracking sys
         d = tuple(a, b, c)
         (a+b+c) / length(d)\n
         
-        speed is milimeters per second
+        speed is milimeters per second\n
+        should return 0 if it performed fine, or -1 if something happened
         """
-        _, _, _, reflection = self.sensorOutput()
+        _, _, fS, reflection = self.sensorOutput()
         deviation = reflection - threshold
+        percentage = 1
         turn = 1.2 * deviation
+        if self.safeMode == True:
+            if fS <= self.slowDownDistance:
+                percentage = (self.slowDownDistance - fS) / (self.slowDownDistance - self.stopDistance)
+
+        if percentage <= 0.1:
+            self.motor.stop()
+            return -1
+        speed *= percentage
         self.motor.drive(speed, turn)
+        return 0
 
     def sensorOutput(self):
         """
@@ -79,5 +101,3 @@ class ROBOT(): #TODO: finish making basic functions, and add a line tracking sys
         frontSide = self.frontSensor.distance()
         lightReflected = self.colorSensor.reflection()
         return leftSide, rightSide, frontSide, lightReflected
-
-    
